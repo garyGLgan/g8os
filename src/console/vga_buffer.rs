930ac416380656f0f9324a56ba53ea0b94294840
Gary Gan <garyglgan@gmail.com>
2020-06-04 14:53:44 +0800

    refactory vga buffer

--- src/console/vga_buffer.rs
+++ src/console/vga_buffer.rs
@@ -8,25 +8,27 @@ use volatile::Volatile;
 // use crate::{serial_print, serial_println};
 
 lazy_static! {
-    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
-        log_area: WriterArea::new(
-            0..(BUFFER_HEIGHT - 2),
-            ColorCode::new(Color::Black, Color::Black)
-        ),
-        input_area: WriterArea::new(
-            0..(BUFFER_HEIGHT - 1),
-            ColorCode::new(Color::Black, Color::Black)
-        ),
-        color_codes: ColorCodes {
-            debug_color: ColorCode::new(Color::Cyan, Color::Black),
-            info_color: ColorCode::new(Color::LightGray, Color::Black),
-            warn_color: ColorCode::new(Color::Yellow, Color::Black),
-            error_color: ColorCode::new(Color::Red, Color::Black),
-            input_color: ColorCode::new(Color::White, Color::Black),
-            blank_color: ColorCode::new(Color::Black, Color::Black),
-        },
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    });
+    pub static ref WRITER: Mutex<Writer> =
+        Mutex::new(Writer {
+            log_row: BufferLine::new(
+                BUFFER_HEIGHT - 2,
+                ColorCode::new(Color::Black, Color::Black)
+            ),
+            input_row: BufferLine::new(
+                BUFFER_HEIGHT - 1,
+                ColorCode::new(Color::Black, Color::Black)
+            ),
+            color_codes: ColorCodes {
+                debug_color: ColorCode::new(Color::Cyan, Color::Black),
+                info_color: ColorCode::new(Color::LightGray, Color::Black),
+                warn_color: ColorCode::new(Color::Yellow, Color::Black),
+                error_color: ColorCode::new(Color::Red, Color::Black),
+                input_color: ColorCode::new(Color::White, Color::Black),
+                blank_color: ColorCode::new(Color::Black, Color::Black),
+            },
+            buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+            scroll_area: ScrollArea::new(0..BUFFER_HEIGHT-3),
+        }); 
 }
 
 #[allow(dead_code)]
@@ -85,34 +87,65 @@ const BUFFER_WIDTH: usize = 80;
 struct Buffer {
     chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct ScrollArea {
+    start: usize, 
+    end: usize,
+}
 
-pub struct WriterArea {
-    row_range: Range<usize>,
+impl ScrollArea {
+    fn new(r: Range<usize>) -> Self {
+        ScrollArea {start: r.start, end: r.end}
+    }
+
+    fn scroll_append_line(&mut self, row: usize, buffer: &mut Buffer) {
+        self.scroll(buffer);
+        self.move_line(row, self.end, buffer);
+    }
+
+    fn scroll(&mut self, buffer: &mut Buffer) {
+        if self.end - self.start >=1 {
+            for row in self.start..self.end {
+                for col in 0..BUFFER_WIDTH {
+                    self.move_line(row+1, row, buffer);
+                }
+            }
+        }
+    }
+
+    fn move_line(&mut self, source: usize, target: usize, buffer: &mut Buffer){
+        for col in 0..BUFFER_WIDTH {
+            let character = buffer.chars[source][col].read();
+            buffer.chars[target][col].write(character);
+        }
+    }
+
+    
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct BufferLine{
+    row: usize,
     column_position: usize,
     blank_color: ColorCode,
 }
 
-impl WriterArea {
-    fn new(r: Range<usize>, blank_color: ColorCode) -> Self {
-        WriterArea {
-            row_range: r,
-            column_position: 0,
-            blank_color,
-        }
+impl BufferLine {
+    fn new(row: usize, blank_color: ColorCode) -> Self {
+        BufferLine{row: row, column_position: 0, blank_color: blank_color}
     }
 
-    fn write_byte(&mut self, byte: u8, color: ColorCode, buffer: &mut Buffer) {
+    fn write_byte(&mut self, byte: u8, color: ColorCode, buffer: &mut Buffer, scroll_area: &mut ScrollArea){
         match byte {
-            b'\n' => self.new_line(buffer),
+            b'\n' => self.refresh(buffer, scroll_area),
             byte => {
                 if self.column_position >= BUFFER_WIDTH {
-                    self.new_line(buffer);
+                    self.refresh(buffer, scroll_area);
                 }
 
-                let row = self.row_range.end;
                 let col = self.column_position;
 
-                buffer.chars[row][col].write(ScreenChar {
+                buffer.chars[self.row][col].write(ScreenChar {
                     ascii_character: byte,
                     color_code: color,
                 });
@@ -121,77 +154,72 @@ impl WriterArea {
         }
     }
 
-    fn write_string(&mut self, s: &str, color: ColorCode, buffer: &mut Buffer) {
-        for byte in s.bytes() {
-            match byte {
-                0x20..=0x7e | b'\n' => self.write_byte(byte, color, buffer),
-                _ => self.write_byte(0xfe, color, buffer),
-            }
-        }
-    }
-
-    fn new_line(&mut self, buffer: &mut Buffer) {
-        for row in (self.row_range.start + 1)..=self.row_range.end {
-            for col in 0..BUFFER_WIDTH {
-                let character = buffer.chars[row][col].read();
-                buffer.chars[row - 1][col].write(character);
-            }
-        }
-        self.clear_row(buffer);
+    fn refresh(&mut self, buffer: &mut Buffer, scroll_area: &mut ScrollArea){
+        scroll_area.scroll_append_line(self.row, buffer);
+        self.clear(buffer);
         self.column_position = 0;
     }
 
-    fn clear_row(&mut self, buffer: &mut Buffer) {
+    fn clear(&mut self, buffer: &mut Buffer) {
         let blank = ScreenChar {
             ascii_character: b' ',
             color_code: self.blank_color,
         };
-
         for col in 0..BUFFER_WIDTH {
-            buffer.chars[self.row_range.end][col].write(blank);
+            buffer.chars[self.row][col].write(blank);
+        }
+    }
+
+    fn write_string(&mut self, s: &str, color: ColorCode, buffer: &mut Buffer, scroll_area: &mut ScrollArea) {
+        for byte in s.bytes() {
+            match byte {
+                0x20..=0x7e | b'\n' => self.write_byte(byte, color, buffer, scroll_area),
+                _ => self.write_byte(0xfe, color, buffer, scroll_area),
+            }
         }
     }
 }
 
 pub struct Writer {
-    input_area: WriterArea,
-    log_area: WriterArea,
+    log_row: BufferLine,
+    input_row: BufferLine,
     color_codes: ColorCodes,
     buffer: &'static mut Buffer,
+    scroll_area: ScrollArea,
 }
 
 impl Writer {
     fn log_byte(&mut self, byte: u8, color: ColorCode) {
-        self.log_area.write_byte(byte, color, self.buffer);
+        self.log_row.write_byte(byte, color, self.buffer,&mut self.scroll_area);
     }
 
     fn input_byte(&mut self, byte: u8, color: ColorCode) {
-        self.input_area.write_byte(byte, color, self.buffer);
+        self.input_row.write_byte(byte, color, self.buffer, &mut self.scroll_area);
     }
 
     pub fn debug(&mut self, s: &str) {
-        self.log_area
-            .write_string(s, self.color_codes.debug_color, self.buffer);
+        self.log_row
+            .write_string(s, self.color_codes.debug_color, self.buffer, &mut self.scroll_area);
     }
 
     pub fn info(&mut self, s: &str) {
-        self.log_area
-            .write_string(s, self.color_codes.info_color, self.buffer);
+        self.log_row
+            .write_string(s, self.color_codes.info_color, self.buffer, &mut self.scroll_area);
     }
 
     pub fn warn(&mut self, s: &str) {
-        self.log_area
-            .write_string(s, self.color_codes.warn_color, self.buffer);
+        self.log_row
+            .write_string(s, self.color_codes.warn_color, self.buffer, &mut self.scroll_area);
     }
 
     pub fn error(&mut self, s: &str) {
-        self.log_area
-            .write_string(s, self.color_codes.error_color, self.buffer);
+        self.log_row
+            .write_string(s, self.color_codes.error_color, self.buffer, &mut self.scroll_area);
     }
 
     pub fn input(&mut self, s: &str) {
-        self.input_area
-            .write_string(s, self.color_codes.input_color, self.buffer);
+        self.input_row
+            .write_string(s, self.color_codes.input_color, self.buffer, &mut self.scroll_area);
     }
 }
 
